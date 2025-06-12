1e7e1716f9f8ca438c392e71301f0af31511357c
Pratham-91 <prathamg08384@gmail.com>
2025-06-12 17:34:33 +0530

    Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useAuthContext } from './context/AuthContext';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Login from './pages/Login/Login';
+import Login from './pages/login/Login';
 import SignUp from './pages/Signup/Signup';
-import Home from './pages/Home/Home';
+import Home from './pages/home/Home';
 import './App.css';
 
 // Custom SVG Logo Component
